7ab33c4ba660373c026cb6a82a3126c0069b8d30
Maen Ibreigheith <m3n.ibregeth@gmail.com>
2020-08-24 02:15:12 +0300

    added the client side and cofigured CRA proxy

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/auth/google">Sign In With Google</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
